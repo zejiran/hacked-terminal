98fdeae130647886bde5b24df8de0dd91c4b1bb4
Juan Alegría <juanszalegria@gmail.com>
2021-07-26 12:00:27 -0500

    Feat: add Linux keyboard layout

---
+++ zejiranLayout/v.0.1/moonlander_zejiran-i_source/config.h
@@ -0,0 +1,11 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef RGB_DISABLE_TIMEOUT
+#define RGB_DISABLE_TIMEOUT 900000
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"jXRZ4/DreMb"
+#define RGB_MATRIX_STARTUP_SPD 60
